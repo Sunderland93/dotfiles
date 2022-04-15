a7e1f468b8e6ccd1102e4702be3c3c9f9dcf276e
Sunderland93 <samoilov.lex@gmail.com>
2022-04-15 09:28:46 +0400

    Fixes, add extra parameters to wob.sh

--- Sway/.config/sway/scripts/wob.sh
+++ Sway/.config/sway/scripts/wob.sh
@@ -12,7 +12,7 @@ is_running_on_this_screen() {
     return 1
 }
 
-new_value=$1 # null or a percent; no checking!!
+new_value=$3 # null or a percent; no checking!!
 
 wob_pipe=~/.cache/$( basename $SWAYSOCK ).wob
 
@@ -20,7 +20,7 @@ wob_pipe=~/.cache/$( basename $SWAYSOCK ).wob
 
 # wob does not appear in $(swaymsg -t get_msg), so:
 is_running_on_this_screen wob || {
-    tail -f $wob_pipe | wob -m 150&
+    tail -f $wob_pipe | wob -m 150 --border-color $1 --bar-color $1 --background-color $2 --anchor top --anchor center --margin 20 &
 }
 
 [[ "$new_value" ]] && echo $new_value > $wob_pipe
